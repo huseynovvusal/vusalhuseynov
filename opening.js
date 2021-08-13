5eae368cf250be3ea605c8f93930af13351aa6c0
Vusal Huseynov <87518350+huseynovvusal@users.noreply.github.com>
2021-08-13 20:22:23 +0400

    Update opening.js

--- opening.js
+++ opening.js
@@ -1,6 +1,6 @@
 const loadingPage = document.querySelector(".loading-page");
 
-window.addEventListener("load", setInterval(loaded, 3000));
+window.addEventListener("load", loaded);
 
 function loaded() {
   loadingPage.classList.add("eventNone");
